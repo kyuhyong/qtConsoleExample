a7d30eeb99bad81b810df59a451f9badd73118fe
kyu <kyuhyong@gmail.com>
2017-04-07 18:36:28 +0900

    Added formation class

--- formation.cpp
+++ formation.cpp
@@ -2,5 +2,22 @@
 
 Formation::Formation()
 {
+    Formation::_UavData.HorizontalSpeed_m_s = 1.0;
+    Formation::_UavData.minimumSafeHeight_m = 3.0;
+    Formation::_UavData.minimumSafeRadious_m = 5.0;
+    Formation::numUavs = 18;
+    Formation::numDecks = 2;
+    Formation::numRows = 3;
+    Formation::numColomns = 3;
+}
+
+Formation::Formation(int num, int rows, int colomn, int decks, double minSafeRadious_m, double minSafeHeight_m)
+{
+    Formation::numUavs = num;
+    Formation::numRows = rows;
+    Formation::numColomns = colomn;
+    Formation::numDecks = decks;
+    Formation::_UavData.minimumSafeRadious_m = minSafeRadious_m;
+    Formation::_UavData.minimumSafeHeight_m = minSafeHeight_m;
 
 }
--- formation.h
+++ formation.h
@@ -5,12 +5,20 @@
 class Formation
 {
 private:
-    int row;
-    int colomn;
-    int decks;
+    int numUavs;
+    int numRows;
+    int numColomns;
+    int numDecks;
+    struct uavData{
+        double minimumSafeRadious_m;
+        double minimumSafeHeight_m;
+        double HorizontalSpeed_m_s;
+        double VerticalSpeed_m_s;
+    }_UavData;
 
 public:
     Formation();
+    Formation(int num, int rows, int colomn, int decks, double minSafeRadious_m, double minSafeHeight_m);
 };
 
 #endif // FORMATION_H
